53a0745837aece22add79dcc314a7fee060e6023
FRIDA LEMKE <frida.lemke@gmx.de>
2024-09-28 22:06:52 +0200

    adjustments to text-content of nav-buttons

--- components/nav-pagination/nav-pagination.js
+++ components/nav-pagination/nav-pagination.js
@@ -1,5 +1,5 @@
 export function updatePagination(pageIndex, maxPage, prevButton, nextButton, paginationElement) {
-    paginationElement.textContent = ` <<< ${pageIndex} / ${maxPage} >>> `;
+    paginationElement.textContent = ` << ${pageIndex} / ${maxPage} >> `;
     prevButton.disabled = pageIndex === 1; 
     nextButton.disabled = pageIndex === maxPage; 
   }
